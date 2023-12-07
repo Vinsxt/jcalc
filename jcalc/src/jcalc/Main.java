2faa0a25af72191bf3d3cec53761db50380e1b65
Vinsx <vincentwhatwat175@gmail.com>
2023-12-07 14:36:43 +0700

    First Re-Commit
    This time try "add to index" before commit.

---
+++ jcalc/src/jcalc/Main.java
@@ -0,0 +1,17 @@
+package jcalc;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		Scanner scanner = new Scanner(System.in);
+		
+		num1 = scanner.nextInt();
+		num2 = scanner.nextInt();
+		System.out.println(num1*num2);
+	
+	}
+
+}
