3b52fd0ee0c24e8f8e508a73c69d5fac221acb41
Vinsx <vincentwhatwat175@gmail.com>
2023-12-07 13:42:45 +0700

    Insert Java Project
    First time inserting a java project created in Eclipse into my newly created repository vinsx-learn.

---
+++ jcalc/src/jcalc/Main.java
@@ -0,0 +1,13 @@
+package jcalc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean hungry = false;
+		if (hungry == false) {
+			System.out.println("get sleep, you full");
+		}
+	}
+
+}
